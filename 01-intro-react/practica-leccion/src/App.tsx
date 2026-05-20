f134daaf039b39cae7d17b713a49418108777278
Mor4n <anm0ran@outlook.com>
2026-05-19 21:44:20 -0700

    Proyecto 01 - Añadida imagen, falta terminar de estilizarlo

--- 01-intro-react/practica-leccion/src/App.tsx
+++ 01-intro-react/practica-leccion/src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import Card from "./components/Card";
-
+import pfp from "./assets/pfp.jpg"
 
 function App() {
 
 
   return (
-    <>
+    <div className="bg-[#F5F5F5]">
+
       <Card
         nombre={"Brayan"}
         profesion={"Estudiante"}
         mensaje={"Holaaa 🐱!"}
+        imagen={pfp}
         
 
       />
-    </>
+
+    </div>
   )
 }
 
--- 01-intro-react/practica-leccion/src/components/Card.tsx
+++ 01-intro-react/practica-leccion/src/components/Card.tsx
@@ -1,20 +1,31 @@
 
+
+
 type CardTypeProps = {
     nombre: string
     profesion: string
     mensaje: string
+    imagen: string
 
 }
 
-function Card( {nombre,profesion,mensaje}: CardTypeProps) {
+function Card( {nombre,profesion,mensaje,imagen}: CardTypeProps) {
 
 
 
   return (
-    <div className="border border-amber-700 border-dotted p-10 w-2xl text-center ">
-        <h2 className="">Mi nombre es: <span className="font-black">{nombre}</span></h2>
-        <h4>Soy <span>{profesion}</span></h4>
-        <p>{mensaje}</p>
+    <div className=" w-2xs h-120 bg-[#FCFCFC] rounded-4xl ">
+
+        <img src={imagen} alt="Imagen de perfil" className="rounded-4xl" />
+        
+        <div>
+            <h2 className="font-black text-2xl">{nombre}</h2>
+            <h4>Soy <span className="italic">{profesion}</span></h4>
+            <p>{mensaje}</p>
+    
+
+        </div>
+
     </div>
   )
 }
