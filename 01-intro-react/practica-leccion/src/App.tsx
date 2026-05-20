cb38e7dbd359be29abc77abf7b73dd88bb291f4a
Mor4n <anm0ran@outlook.com>
2026-05-19 21:22:10 -0700

    Proyecto 01 - Creación de componente Card y renderizado

--- 01-intro-react/practica-leccion/src/App.tsx
+++ 01-intro-react/practica-leccion/src/App.tsx
@@ -1,10 +1,11 @@
+import Card from "./components/Card";
 
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <Card/>
     </>
   )
 }
---
+++ 01-intro-react/practica-leccion/src/components/Card.tsx
@@ -0,0 +1,20 @@
+
+
+
+function Card() {
+
+    const nombre = "Brayan";
+    const profesion = "Estudiante";
+    const mensaje = "Holaaa 🐱!";
+
+
+  return (
+    <div className="">
+        <h2>{nombre}</h2>
+        <h4>{profesion}</h4>
+        <p>{mensaje}</p>
+    </div>
+  )
+}
+
+export default Card
